db0fa530f251462cb6eb200f1b34950616a2b020
reni17 <69053305+reni17@users.noreply.github.com>
2024-03-27 11:02:39 +0200

    Create custom server action abstraction combined with hook

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,17 +1,29 @@
 "use client";
 
-import { useFormState } from "react-dom";
-import { State, create } from "@/actions/create-board";
-import { Button } from "@/components/ui/button";
+import {  createBoard } from "@/actions/create-board/index";
 import { FormInput } from "./input";
 import { FormButton } from "./form-button";
+import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
-  const initialState : State = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(create, initialState)
+  const {execute, fieldErrors} = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, 'success');
+    },
+    onError: (error) => {
+      console.log(error);
+      
+    }
+  })
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string
+
+    execute({title})
+  }
   return (
-    <form action={dispatch}>
-      <FormInput errors={state?.errors}/>
+    <form action={onSubmit}>
+      <FormInput errors={fieldErrors}/>
       <FormButton/>
     </form>
   );
